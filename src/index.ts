0da5359b2c5a6e4db407fd81e927a6b485569659
Captain_Turtle <133596616+heren1heren@users.noreply.github.com>
2024-03-12 09:30:10 -0600

    I am done with this

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 // import './style.css';
 import './style.scss';
 
+// currently don't know how to store and displace shortest paths. should not try to implement both. Waiting for later implementation;
+
 class Node {
+  parent: Node;
+  next0: Node;
   next1: Node;
   next2: Node;
   next3: Node;
@@ -9,9 +13,11 @@ class Node {
   next5: Node;
   next6: Node;
   next7: Node;
-  next8: Node;
+
   coordinate: number[];
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, parent: Node) {
+    this.parent = parent;
+    this.next0 = null;
     this.next1 = null;
     this.next2 = null;
     this.next3 = null;
@@ -19,7 +25,6 @@ class Node {
     this.next5 = null;
     this.next6 = null;
     this.next7 = null;
-    this.next8 = null;
     this.coordinate = [x, y];
   }
 }
@@ -28,61 +33,53 @@ const dx = [-2, -1, 1, 2, -2, -1, 2, 1]; // possible x moves
 const dy = [-1, -2, 2, 1, 1, 2, -1, -2]; // possible y moves
 function knightMove(startPoint: number[], endPoint: number[]) {
   //
-  console.log(startPoint);
+
   const visitedValues = new Set([startPoint]);
-  const queue = [startPoint];
-  const rootNode = new Node(startPoint[0], startPoint[1]);
-  let index = 1;
+  const rootNode = new Node(startPoint[0], startPoint[1], null);
+  const queue = [rootNode];
+  const stepsByString = '';
   while (queue.length > 0) {
-    const coordinate = queue.shift();
+    const current = queue.shift();
+
     // how to move down one level?
 
-    /**
-     *  check  where is the first child before the next child is null
-     *  access that child
-     * else if all child is already access
-     * access the child of child and repeat the process
-     */
-
-    let current = rootNode;
-    if (index === 8) {
-      index = 0;
-      current = current.next1;
-    }
-    if (index === 0) {
-    } else if (current[`next${index}`] !== null) {
-      while (current[`next${index}`] === null) {
-        index++;
-      }
-      current = current[`next${index}`];
-    }
-
     for (let i = 0; i < 8; i++) {
-      current[`next${i + 1}`] = new Node(
+      current[`next${i}`] = new Node(
         current.coordinate[0] + dx[i],
-        current.coordinate[1] + dy[i]
+        current.coordinate[1] + dy[i],
+        current
       );
+      console.log(current[`next${i}`].coordinate);
+      console.log(isChildOnTheBoard(current[`next${i}`]));
+      console.log(!isChildExisted(current[`next${i}`]));
 
       if (
-        isChildOnTheBoard(current[`next${i + 1}`]) &&
-        !isChildExisted(current[`next${i + 1}`])
+        isChildOnTheBoard(current[`next${i}`]) &&
+        !isChildExisted(current[`next${i}`])
       ) {
         // this current child is on the boarld and the child is not already existed
-        visitedValues.add(current[`next${i + 1}`].coordinate);
-        // queue.push(current[`next${i + 1}`].coordinate);
+        visitedValues.add(current[`next${i}`].coordinate.toString());
+
+        queue.push(current[`next${i}`]);
       } else {
-        current[`next${i + 1}`] = null;
+        current[`next${i}`] = null;
       }
     }
 
-    if (current.coordinate === endPoint) {
-      console.log(current);
-      console.log('found it');
+    if (
+      current.coordinate[0] === endPoint[0] &&
+      current.coordinate[1] === endPoint[1]
+    ) {
+      stepsByString += `[${current.coordinate}] -> `;
+      while (current.parent !== null) {
+        stepsByString += `[${current.parent.coordinate}] -> `;
+        current = current.parent;
+      }
+      console.log(stepsByString.slice(0, stepsByString.length - 3));
 
       break;
     }
-    index++;
-    console.log(queue);
+    console.log(current);
   }
 
   function isChildOnTheBoard(node: Node) {
@@ -98,7 +95,9 @@ function knightMove(startPoint: number[], endPoint: number[]) {
   }
 
   function isChildExisted(node: Node) {
-    if (visitedValues.has(node.coordinate)) return true;
+    const string = node.coordinate.toString();
+
+    if (visitedValues.has(string)) return true;
 
     return false;
   }
@@ -108,6 +107,6 @@ function knightMove(startPoint: number[], endPoint: number[]) {
 // visitedValues.add(2);
 // console.log(visitedValues.has(2));
 
-knightMove([2, 1], [3, 5]);
+knightMove([2, 1], [7, 7]);
 
 //instead of implementing an array
